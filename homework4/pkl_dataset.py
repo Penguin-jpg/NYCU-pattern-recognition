9f8e215e130610946f4f2ef64631becd51dbd6bb
Penguin.jpg <sky026912@gmail.com>
2024-05-29 10:41:08 +0800

    add pkl_dataset.py

---
+++ homework4/pkl_dataset.py
@@ -0,0 +1,67 @@
+import pickle
+from torch.utils import data
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
+import os
+from glob import glob
+import torch
+import torchvision.transforms as T
+import numpy as np
+
+
+class Bag:
+    def __init__(self, pkl_path, label):
+        self.pkl_path = pkl_path
+        self.label = label
+
+
+class PickleTrainDataest(Dataset):
+    def __init__(self, image_dir, transform=None):
+        self.bags = []
+        for pkl_path in glob(os.path.join(image_dir, "train", "class_0", "*.pkl")):
+            self.bags.append(Bag(pkl_path, 0))
+        for pkl_path in glob(os.path.join(image_dir, "train", "class_1", "*.pkl")):
+            self.bags.append(Bag(pkl_path, 1))
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.bags)
+
+    def __getitem__(self, index):
+        bag = self.bags[index]
+
+        # each bag contains 256 images with shape (128, 128, 3)
+        # shape: [256, 128, 128, 3]
+        with open(bag.pkl_path, "rb") as file:
+            data = pickle.load(file)
+
+        images = []
+        if self.transform is not None:
+            # data = torch.from_numpy(data).permute(0, 3, 1, 2).to(dtype=torch.float32)
+            for i in range(data.shape[0]):
+                images.append(self.transform(data[i]))
+        else:
+            to_tensor = T.ToTensor()
+            for i in range(data.shape[0]):
+                images.append(to_tensor(data[i]))
+
+        images = torch.stack(images)
+        label = torch.as_tensor([bag.label])
+
+        return images, label
+
+
+if __name__ == "__main__":
+    transform = T.Compose(
+        [
+            T.ToTensor(),
+            T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ]
+    )
+    dataset = PickleTrainDataest(image_dir="dataset", transform=transform)
+    dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
+    print(len(dataset))
+
+    for bag, label in dataloader:
+        print(bag)
+        print(label)
